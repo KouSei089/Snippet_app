5f67509f7e12f1337a368913a1a1c5ec1cf2edd7
KouSei089 <qq00891829@icloud.com>
2021-05-13 23:25:27 +0900

    helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,6 +3,5 @@ require_relative '../config/environment'
 require 'rails/test_help'
 
 class ActiveSupport::TestCase
-  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
 end
